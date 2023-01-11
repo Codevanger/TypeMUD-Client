b4327800142ea5cc2bc4ca55a1914d801008bd77
Igor Rybakov <codevanger@gmail.com>
2023-01-11 13:53:49 +0200

    Added level to the character

--- src/app/types/character.ts
+++ src/app/types/character.ts
@@ -5,4 +5,23 @@ export declare interface Character {
   name: string;
   location: number;
   user: User;
+  level: number;
+  experience: number;
+  health: number;
+  stamina: number;
+  mana: number;
+  money: number;
+  friends: number[];
+  skills: number[];
+  inventory: number[];
+  stats: Stats;
+}
+
+export declare interface Stats {
+  str: number;
+  dex: number;
+  vit: number;
+  end: number;
+  int: number;
+  wis: number;
 }
